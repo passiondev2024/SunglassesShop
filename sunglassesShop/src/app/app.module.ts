76bc01aa3284fd4f7501650e7bdb0d34d25d5817
kanandev2024 <kananramazandev@gmail.com>
2024-03-07 20:32:51 +0200

    create core module and header component

--- sunglassesShop/src/app/app.module.ts
+++ sunglassesShop/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
