d3d1bc3a548d7c94df3998bced9e302933fe57b6
kanandev2024 <kananramazandev@gmail.com>
2024-03-08 09:59:20 +0200

    create home component

--- sunglassesShop/src/app/app.module.ts
+++ sunglassesShop/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
