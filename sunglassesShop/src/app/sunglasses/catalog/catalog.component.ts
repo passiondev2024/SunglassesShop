5e3db6b135c453e410e31d7f39c46256d07e8b90
kanandev2024 <kananramazandev@gmail.com>
2024-03-08 14:46:50 +0200

    updated catalog component

--- sunglassesShop/src/app/sunglasses/catalog/catalog.component.ts
+++ sunglassesShop/src/app/sunglasses/catalog/catalog.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SunglassesService } from '../sunglasses.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  isEmptyCollection: boolean = false
+  emptyCollectionMessage: string | undefined
 
- 
+  constructor(private sunglassesService: SunglassesService) { }
+
+  ngOnInit(): void {
+    this.sunglassesService.getSunglasses().subscribe({
+      next: sunglasses => {
+        console.log(sunglasses)
+      },
+      error: (error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          this.isEmptyCollection = true
+          this.emptyCollectionMessage = 'No sunglasses yet'
+        } else {
+          alert(error.statusText)
+        }
+      }
+    })
+  }
 }
--- sunglassesShop/src/app/sunglasses/sunglasses-routing.module.ts
+++ sunglassesShop/src/app/sunglasses/sunglasses-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './create/create.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 const routes: Routes = [
-{path: 'create', component: CreateComponent}
+{path: 'create', component: CreateComponent},
+{path: 'catalog', component: CatalogComponent}
 ];
 
 @NgModule({
@@ -11,3 +13,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class SunglassesRoutingModule { }
+
