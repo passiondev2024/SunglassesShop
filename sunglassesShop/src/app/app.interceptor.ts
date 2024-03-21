d733e6cd305ab42998932ba904e36c892a79e365
kanandev2024 <kananramazandev@gmail.com>
2024-03-21 11:29:15 +0200

    update app interceptop - set Header with X-Authorization: token

--- sunglassesShop/src/app/app.interceptor.ts
+++ sunglassesShop/src/app/app.interceptor.ts
@@ -8,18 +8,30 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+import { AuthenticationService } from './authentication/authentication.service';
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
 
   apiUrl: string = environment.apiUrl
 
+  constructor(private authenticationService: AuthenticationService) { }
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if(request.url.startsWith('/api')){
+    if (request.url.startsWith('/api')) {
       request = request.clone({
         url: request.url.replace('/api', this.apiUrl)
       })
-    } 
+
+      if (this.authenticationService.user) {
+        const token = this.authenticationService.user.accessToken
+        request = request.clone({
+          setHeaders: {
+            'X-Authorization': token
+          }
+        })
+      }
+    }
 
     return next.handle(request);
   }
@@ -30,3 +42,4 @@ export const AppInterceptorProvider: Provider = {
   useClass: AppInterceptor,
   multi: true
 }
+
